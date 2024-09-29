e6754366c6a74bbe0169c3618ab90f7f42b69aa0
pawan-live <pawan.senpura@gmail.com>
2024-09-29 12:33:37 +0530

    Add image import

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { BlogPosts } from "app/components/posts";
 import Image from "next/image";
+import profilePic from "../public/image.jpg";
 
 export default function Page() {
   return (
     <section>
       <div className="flex gap-x-4 items-center flex-row mb-10">
         <Image
-          src={"/image.jpg"}
+          src={profilePic}
           alt="profile pic"
           width={100}
           height={100}
